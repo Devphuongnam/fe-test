c34d1ffe7c88f7aba4708475c93c53112849c9a5
NamCP <chuleeminh265@gmail.com>
2024-10-31 23:07:38 +0700

    ad

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,88 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-  useLocation,
-  HashRouter,
-} from "react-router-dom";
-import { Frame, Navigation, Page } from "@shopify/polaris";
-import Dashboard from "./components/Dashboard";
-import Products from "./components/Products";
-import Settings from "./components/Settings";
-import { HomeIcon, OrderIcon, ProductIcon } from "@shopify/polaris-icons";
+import anh from "./assets/anh.png";
 
 function App() {
   return (
-    <Router>
-      <Frame>
-        <Page fullWidth>
-          <div style={{ display: "flex", height: "100vh" }}>
-            <CustomNavigation />
-
-            <div style={{ marginLeft: "20px", flex: 1 }}>
-              <Routes>
-                <Route path="/" element={<Navigate to="/dashboard" />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/products" element={<Products />} />
-                <Route path="/settings" element={<Settings />} />
-              </Routes>
-            </div>
-          </div>
-        </Page>
-      </Frame>
-    </Router>
-  );
-}
-
-function CustomNavigation() {
-  const location = useLocation();
-
-  return (
-    <Navigation location={location.pathname}>
-      <Navigation.Section
-        items={[
-          {
-            url: "dashboard",
-            label: "Dashboard",
-            icon: HomeIcon,
-            selected: location.pathname === "/dashboard",
-          },
-          {
-            url: "products",
-            label: "Products",
-            icon: OrderIcon,
-            selected: location.pathname === "/products",
-          },
-          {
-            url: "settings",
-            label: "Settings",
-            icon: ProductIcon,
-            selected: location.pathname === "/settings",
-          },
-        ]}
-      />
-    </Navigation>
+    <div>
+      <div
+        style={{
+          textAlign: "center",
+          fontSize: "20px",
+          padding: "10px",
+          fontWeight: "700",
+          color: "red",
+        }}
+      >
+        Anh yêu em, Thảo Ngọc ạ !!!
+      </div>
+      <img style={{ width: "100%" }} src={anh} alt="" />
+      <div
+        style={{
+          textAlign: "center",
+          fontSize: "20px",
+          padding: "10px",
+          fontWeight: "700",
+          color: "red",
+        }}
+      >
+        Anh yêu em, Thảo Ngọc ạ !!!
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+          fontSize: "20px",
+          padding: "10px",
+          fontWeight: "700",
+          color: "red",
+        }}
+      >
+        Anh yêu em, Thảo Ngọc ạ !!!
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+          fontSize: "20px",
+          padding: "10px",
+          fontWeight: "700",
+          color: "red",
+        }}
+      >
+        Anh yêu em, Thảo Ngọc ạ !!!
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+          fontSize: "20px",
+          padding: "10px",
+          fontWeight: "700",
+          color: "red",
+        }}
+      >
+        Anh yêu em, Thảo Ngọc ạ !!!
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+          fontSize: "20px",
+          padding: "10px",
+          fontWeight: "700",
+          color: "red",
+        }}
+      >
+        Anh yêu em, Thảo Ngọc ạ !!!
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+          fontSize: "20px",
+          padding: "10px",
+          fontWeight: "700",
+          color: "red",
+        }}
+      >
+        Anh yêu em, Thảo Ngọc ạ !!!
+      </div>
+    </div>
   );
 }
 
